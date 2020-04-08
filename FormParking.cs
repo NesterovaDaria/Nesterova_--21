36cee42d028b439c4b3965b07191e1312821bc7a
NesterovaDaria <60694046+NesterovaDaria@users.noreply.github.com>
2020-04-09 00:26:29 +0400

    Update FormParking.cs

--- FormParking.cs
+++ FormParking.cs
@@ -12,64 +12,97 @@ namespace WindowsFormsAppCruiser
 {
     public partial class FormParking : Form
     {
-        Parking<ITransport> parking;
+        MultiLevelParking parking;
+        // Количество уровней-парковок 
+        private const int countLevel = 5;
         public FormParking()
         {
             InitializeComponent();
-            parking = new Parking<ITransport>(15, pictureBoxParking.Width, pictureBoxParking.Height);
-            Draw();
+            parking = new MultiLevelParking(countLevel, pictureBoxParking.Width, pictureBoxParking.Height);
+            //заполнение listBox
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxLevels.SelectedIndex = 0;
         }
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
-            Graphics gr = Graphics.FromImage(bmp);
-            parking.Draw(gr);
-            pictureBoxParking.Image = bmp;
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                //если выбран один из пуктов в listBox (при старте программы ни один пункт не будет выбран
+                //и может возникнуть ошибка, если мы попытаемся обратиться к элементу listBox)
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking[listBoxLevels.SelectedIndex].Draw(gr);
+                pictureBoxParking.Image = bmp;
+            }
         }
         private void buttonSetShip_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var ship = new Ship(200, 1150, dialog.Color);
-                int place = parking + ship;
-                Draw();
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var shep = new Ship(100, 1000, dialog.Color);
+                    int place = parking[listBoxLevels.SelectedIndex] + shep;
+                    if (place == -1)
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    Draw();
+                }
             }
         }
         private void buttonSetCruiser_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    var ship = new Cruiser(100, 1000, dialog.Color, dialogDop.Color, dialogDop.Color, true, true, true, true);
-                    int place = parking + ship;
-                    Draw();
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var shep = new Cruiser(100, 1000, dialog.Color, dialogDop.Color,dialogDop.Color, true, true, true, true);
+                        int place = parking[listBoxLevels.SelectedIndex] + shep;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        Draw();
+                    }
                 }
             }
         }
         private void buttonTakeShip_Click(object sender, EventArgs e)
         {
-            if (maskedTextBox.Text != "")
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var ship = parking - Convert.ToInt32(maskedTextBox.Text);
-                if (ship != null)
+                if (maskedTextBox.Text != "")
                 {
-                    Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width, pictureBoxTakeShip.Height);
-                    Graphics gr = Graphics.FromImage(bmp);
-                    ship.SetPosition(15, 15, pictureBoxTakeShip.Width, pictureBoxTakeShip.Height);
-                    ship.Draw(gr);
-                    pictureBoxTakeShip.Image = bmp;
+                    var shep = parking[listBoxLevels.SelectedIndex] - Convert.ToInt32(maskedTextBox.Text);
+                    if (shep != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width, pictureBoxTakeShip.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        shep.SetPosition(15, 15, pictureBoxTakeShip.Width, pictureBoxTakeShip.Height);
+                        shep.Draw(gr);
+                        pictureBoxTakeShip.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width, pictureBoxTakeShip.Height);
+                        pictureBoxTakeShip.Image = bmp;
+                    }
+                    Draw();
                 }
-                else
-                {
-                    Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width, pictureBoxTakeShip.Height);
-                    pictureBoxTakeShip.Image = bmp;
-                }
-                Draw();
             }
+        }
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
         }
     }
 }
